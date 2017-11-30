cef5d73333388d4a2ea2cef21a1e0cb1542b2ecd
Pedro Cunial <pedrocc4@al.insper.edu.br>
2017-11-30 11:42:36 -0200

    Quick commit - qui nov 30 11:42:36 -02 2017

--- pccli.py
+++ pccli.py
@@ -1,4 +1,32 @@
 #!/bin/python3
 import boto3
+import pickle
+import click
 
 from botocore.exceptions import ClientError
+from pathlib import Path
+from pedro import create_instance
+from util.constants import *
+
+
+def _get_service_data():
+    try:
+        d = pickle.load(Path('{}/.pccdata.p'.format(Path.home())).open('rb'))
+        return d
+    except IOError as e:
+        print('Data file ({}/.pccdata.p) not found, are you running instances already?'\
+              .format(Path.home()))
+        return False
+
+
+@click.command()
+@click.option('--add', '-a', default=1, type=int)
+def add_instances(add):
+    data = _get_service_data()
+    ec2 = boto3.client('ec2')
+    elb = boto3.client('elb')
+
+    instances = create_instance(ec2, IMAGE_ID, INSTANCE_TYPE, data['sg_id'],
+                                add, ZONES)
+    if len(instances) < add:
+        print('Failed to create instances')
